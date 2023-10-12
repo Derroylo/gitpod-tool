4e841540135c797f839c39c1f7e579871b0e89d5
Carsten Lohmann <CLohmann@gmx.net>
2023-10-12 22:14:34 +0000

    Install packages that are available in the latest php version but not yet installed in the new version when changing php versions

--- helper/PhpHelper.cs
+++ helper/PhpHelper.cs
@@ -92,7 +92,53 @@ namespace Gitpod.Tool.Helper
 
                     AnsiConsole.MarkupLine("Validating that the new version has been set...[green1]Success[/]");
 
-                    ctx.Status("Checking if file with additional packages exists...");
+                    ctx.Status("Checking if the active php version has the same packages installed as the newest one...");
+
+                    // Check if the newVersion is not the latest one and verify all packages are installed in the newVersion too
+                    if (newVersion != availablePhpVersions[0]) {
+                        // Read currently installed packages
+                        var installedPackages = ExecCommand.Exec("apt list --installed");
+
+                        var packagesList = installedPackages.Split("\n");
+                        var packagesCleaned = new List<string>();
+
+                        foreach (string package in packagesList) {
+                            var tmp = package.Split("/");
+
+                            packagesCleaned.Add(tmp[0].Trim());
+                        }
+
+                        var latestPackages = packagesCleaned.Where(p => p.Contains("php" + availablePhpVersions[0] + "-")).ToArray();
+
+                        var newVersionPackages = packagesCleaned.Where(p => p.Contains("php" + newVersion + "-")).ToArray();
+
+                        var missingPackages = latestPackages.Where(p => !newVersionPackages.Contains(p.Replace("php" + availablePhpVersions[0] + "-", "php" + newVersion + "-"))).ToArray();
+
+                        if (missingPackages.Length > 0) {
+                            var updateRes = ExecCommand.Exec("sudo apt-get update");
+                            AnsiConsole.MarkupLine("Updating package manager list...[green1]Done[/]");
+
+                            if (isDebug) {
+                                AnsiConsole.WriteLine(updateRes);
+                            }
+
+                            string packages = string.Join(" ", missingPackages).Replace("php" + availablePhpVersions[0] + "-", "php" + newVersion + "-");
+
+                            var installRes = ExecCommand.Exec("sudo apt-get install -y " + packages);
+                            AnsiConsole.MarkupLine("Installing packages...[green1]Done[/]");
+                            
+                            if (isDebug) {
+                                AnsiConsole.WriteLine(installRes);
+                            }
+
+                            // Restart webserver so newly installed packages are available
+                            ExecCommand.Exec("apachectl stop");
+                            ExecCommand.Exec("apachectl start");
+                            AnsiConsole.MarkupLine("Restarting apache...[green1]Success[/]");
+                        }
+                    }
+
+                    ctx.Status("Checking if additional packages are defined in the config file...");
 
                     if (GptConfigHelper.Config?.Php?.Packages?.Count > 0) {
                         // Read currently installed packages
@@ -135,7 +181,7 @@ namespace Gitpod.Tool.Helper
                             AnsiConsole.MarkupLine("Restarting apache...[green1]Success[/]");
                         }
                     } else {
-                        AnsiConsole.MarkupLine("Checking if file with additional packages exists...[cyan3]Not found[/]");
+                        AnsiConsole.MarkupLine("Checking if additional packages are defined in the config file...[cyan3]Not found[/]");
                     }
 
                     ctx.Status("Saving the new active version so it can be restored...");
