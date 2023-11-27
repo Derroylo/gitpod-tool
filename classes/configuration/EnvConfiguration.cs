1bd15675706b56ed1adb727fd853e39562948a59
Carsten Lohmann <CLohmann@gmx.net>
2023-11-27 23:49:25 +0000

    Added a command to add new env variables

--- classes/configuration/EnvConfiguration.cs
+++ classes/configuration/EnvConfiguration.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Gitpod.Tool.Classes.Configuration
--- commands/environment/AddEnvCommand.cs
+++ commands/environment/AddEnvCommand.cs
@@ -2,10 +2,13 @@ using System.Collections.Generic;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using Gitpod.Tool.Helper.Internal.Config.Sections;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System;
 
 namespace Gitpod.Tool.Commands.Environment
 {
-    class AddEnvCommand : Command<AddEnvCommand.Settings>
+    partial class AddEnvCommand : Command<AddEnvCommand.Settings>
     {
         public class Settings : CommandSettings
         {
@@ -23,7 +26,60 @@ namespace Gitpod.Tool.Commands.Environment
                     .AddChoices(envTypes)
             );
 
+            if (envType == "Variable") {
+                AddVariable();
+            }
+
             return 0;
+        }
+
+        [GeneratedRegex(@"^([a-z0-9-_]+)$")]
+        private static partial Regex EnvNameMatchRegex();
+
+        private void AddVariable()
+        {
+            AnsiConsole.MarkupLine("[red]Don´t save any sensible informations via this command![/]");
+            AnsiConsole.MarkupLine("[red]For sensible data, use the gp env command[/]");
+
+            bool finished = false;
+
+            do {
+                var envVarName = string.Empty;
+                bool varNameInvalid = false;
+
+                do {
+                    varNameInvalid = false;
+
+                    envVarName = AnsiConsole.Ask<string>("What is the name of the variable?");
+
+                    if (!EnvNameMatchRegex().Match(envVarName).Success) {
+                        AnsiConsole.MarkupLine("[red]The variable name should only consist of the following characters: a-z 0-9 - _[/]");
+                        varNameInvalid = true;
+                    }
+                } while (varNameInvalid);
+                
+                var envVarContent = AnsiConsole.Ask<string>("What is the content of the variable?");
+
+                if (EnvironmentConfig.Variables.Keys.Contains(envVarName, StringComparer.OrdinalIgnoreCase)) {
+                    if (AnsiConsole.Confirm("A variable with that name already exists. Do you want to replace it?", false)) {
+                        var key = EnvironmentConfig.Variables.Keys.FirstOrDefault(x => x.Equals(envVarName, StringComparison.OrdinalIgnoreCase));
+
+                        EnvironmentConfig.Variables[key] = envVarContent;
+                        EnvironmentConfig.ConfigUpdated = true;
+
+                        AnsiConsole.MarkupLine("[green]The variable has been update.[/]");
+                    }
+                } else {
+                    EnvironmentConfig.Variables.Add(envVarName, envVarContent);
+                    EnvironmentConfig.ConfigUpdated = true;
+
+                    AnsiConsole.MarkupLine("[green]The variable has been added.[/]");
+                }
+                
+                if (!AnsiConsole.Confirm("Do you want to add more variables?", false)) {
+                    finished = true;
+                }
+            } while (!finished);
         }
     }
 }
